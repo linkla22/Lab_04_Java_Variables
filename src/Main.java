0373871877e4a2565f43d2c3d5b1e2bb1bc63efc
linkla22 <linkla@mail.uc.edu>
2023-11-30 00:57:23 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int intOperandA = 71;
+        int intOperandB = 26;
+        int intSum, intProduct, intDifference, intQuotient, intModulo;
+
+
+        intSum = intOperandA + intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+
+
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+        System.out.println("The difference between " + intOperandA + " and " + intOperandB + " is " + intDifference);
+        System.out.println("The quotient of " + intOperandA + " divided by " + intOperandB + " is " + intQuotient);
+        System.out.println("The remainder when " + intOperandA + " is divided by " + intOperandB + " is " + intModulo);
+
+        double doubleOperandA = 45.65;
+        double doubleOperandB = 12.46;
+        double doubleSum, doubleProduct, doubleDifference, doubleQuotient;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+        System.out.println("The difference between " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+        System.out.println("The quotient of " + doubleOperandA + " divided by " + doubleOperandB + " is " + doubleQuotient);
+
+        // Next Part in the Assignment
+        int numberOfShirts = 13;
+        double chipotleMealCost = 15.75;
+        String fullName = "Luke Link";
+        double shoeSize = 13.5;
+        int birthMonth = 10;
+
+        System.out.println("The number of shirts in my closet is: " + numberOfShirts);
+        System.out.println("The price of my chipotle meal is: $" + chipotleMealCost);
+        System.out.println("My shoe size is: " + shoeSize);
+        System.out.println("My birth month is: " + birthMonth);
+        System.out.println("My full name is: " + fullName);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
